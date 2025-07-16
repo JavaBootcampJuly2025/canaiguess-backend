65650cb89651d881b85095e6189e9d5c660cc68c
NiklavsV <niklavs.veiksans@edu.rtu.lv>
2025-07-16 17:00:33 +0300

    feat: made DTO's for answer validation

---
+++ src/main/java/com/canaiguess/api/dto/ResultRequestDTO.java
@@ -0,0 +1,21 @@
+package com.canaiguess.api.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class ResultRequestDTO {
+    private List<String> imageFilenames;
+    private List<String> guesses;
+}
+
