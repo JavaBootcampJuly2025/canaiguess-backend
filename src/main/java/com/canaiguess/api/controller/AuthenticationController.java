3698787dd94b667a414cc1a7c57b669318e805e4
Daivaras L <daivaras38@gmail.com>
2025-07-17 19:10:49 +0300

    Moved secret key to local secret-key.env file. Commented duplicated dependency in pom, added registration field validation with hibernate-validator.

--- src/main/java/com/canaiguess/api/controller/AuthenticationController.java
+++ src/main/java/com/canaiguess/api/controller/AuthenticationController.java
@@ -6,6 +6,7 @@ import com.canaiguess.api.dto.RegisterRequest;
 import com.canaiguess.api.service.AuthenticationService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,7 +28,7 @@ public class AuthenticationController {
             description = "Creates a new user and returns a JWT token."
     )
     public ResponseEntity<AuthenticationResponse> register(
-            @RequestBody RegisterRequest request
+            @RequestBody @Valid RegisterRequest request
     ) {
         return ResponseEntity.ok(service.register(request));
     }
--- src/main/java/com/canaiguess/api/dto/RegisterRequest.java
+++ src/main/java/com/canaiguess/api/dto/RegisterRequest.java
@@ -1,5 +1,6 @@
 package com.canaiguess.api.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,7 +12,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RegisterRequest {
 
+    @NotEmpty(message = "Username is mandatory")
+    @NotBlank(message = "Username is mandatory")
     private String username;
+    @Email(message = "Email is not properly formatted")
+    @NotEmpty(message = "Email is mandatory")
+    @NotBlank(message = "Email is mandatory")
     private String email;
+    @NotEmpty(message = "Password is mandatory")
+    @NotBlank(message = "Password is mandatory")
+    @Size(min = 8, message = "Minimum password length is 8 characters")
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$",
+            message = "Minimum eight characters, at least one letter and one number"
+    )
     private String password;
 }
