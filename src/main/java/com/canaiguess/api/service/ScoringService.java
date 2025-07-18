06984b838e909d745345ce5d4750c11e04e312d2
Henrijs Kravals <henrijskravals@gmail.com>
2025-07-18 11:10:57 +0300

    fix: update user score after last guess

--- src/main/java/com/canaiguess/api/service/ScoringService.java
+++ src/main/java/com/canaiguess/api/service/ScoringService.java
@@ -5,7 +5,6 @@ import com.canaiguess.api.model.ImageGame;
 import com.canaiguess.api.model.User;
 import com.canaiguess.api.repository.ImageGameRepository;
 import com.canaiguess.api.repository.UserRepository;
-import com.canaiguess.api.repository.GameRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,8 +16,7 @@ public class ScoringService {
     private final UserRepository userRepository;
 
     public ScoringService(ImageGameRepository imageGameRepository,
-                          UserRepository userRepository,
-                          GameRepository gameRepository) {
+                          UserRepository userRepository) {
         this.imageGameRepository = imageGameRepository;
         this.userRepository = userRepository;
     }
@@ -31,9 +29,11 @@ public class ScoringService {
                 .filter(ImageGame::isUserGuessedCorrectly)
                 .count();
 
-        double batchSizeEffect = 1 + (1 - Math.exp((double) -game.getBatchSize() / 5)); // 1 to 2 (softmax)
-        double difficultyEffect = game.getDifficulty() / 100.0; // 0 to 1 (proportional)
-        int score = (int) Math.round(correct * difficultyEffect * batchSizeEffect * 10); // 0 to [batchSize * batchCount * 20]
+//        double batchSizeEffect = 1 + (1 - Math.exp((double) -game.getBatchSize() / 5)); // 1 to 2 (softmax)
+//        double difficultyEffect = game.getDifficulty() / 100.0; // 0 to 1 (proportional)
+//        int score = (int) Math.round(correct * difficultyEffect * batchSizeEffect * 10); // 0 to [batchSize * batchCount * 20]
+
+        int score = 10;
 
         user.setScore(user.getScore() + score);
         userRepository.save(user);
