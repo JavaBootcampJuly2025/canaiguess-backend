77487d5228c41d6cea48d3db7586fcc4ed248cb6
Henrijs Kravals <henrijskravals@gmail.com>
2025-07-23 20:22:08 +0300

    feat: implement image upload endpoints end service for using R2 Object storage SDK

--- src/main/java/com/canaiguess/api/controller/ImageController.java
+++ src/main/java/com/canaiguess/api/controller/ImageController.java
@@ -2,34 +2,34 @@ package com.canaiguess.api.controller;
 
 import com.canaiguess.api.dto.HintResponseDTO;
 import com.canaiguess.api.dto.SubmitReportRequestDTO;
+import com.canaiguess.api.dto.UploadImageRequestDTO;
 import com.canaiguess.api.model.Image;
 import com.canaiguess.api.model.User;
 import com.canaiguess.api.repository.ImageRepository;
 import com.canaiguess.api.service.GeminiService;
 import com.canaiguess.api.service.ImageReportService;
+import com.canaiguess.api.service.R2UploadService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("api/image")
 @Tag(name = "Image", description = "Image operations including AI analysis")
+@AllArgsConstructor
 public class ImageController {
 
     private final GeminiService geminiService;
     private final ImageRepository imageRepository;
     private final ImageReportService imageReportService;
-
-    public ImageController(GeminiService geminiService,
-                           ImageRepository imageRepository,
-                           ImageReportService imageReportService) {
-        this.geminiService = geminiService;
-        this.imageRepository = imageRepository;
-        this.imageReportService = imageReportService;
-    }
+    private final R2UploadService uploadService;
 
     @Operation(summary = "Get a hint for an image")
     @PostMapping("/{imageId}/hint")
@@ -55,4 +55,15 @@ public class ImageController {
         return ResponseEntity.noContent().build();
 
     }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @Operation(summary = "Upload an image (ADMIN)")
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadImage(@RequestParam UploadImageRequestDTO request,
+                                              @AuthenticationPrincipal User user) {
+
+        String url = uploadService.uploadAndSave(request.getFile(), request.isFake());
+        return ResponseEntity.ok(url);
+    }
+
 }
