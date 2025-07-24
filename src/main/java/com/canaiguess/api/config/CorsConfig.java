124f008763544e5158b4a7fd7973a8cfb3cf85ca
Julija Tatarinceva <huducn@gmail.com>
2025-07-24 11:27:21 +0300

    fix: add a global cors filter

--- src/main/java/com/canaiguess/api/config/CorsConfig.java
+++ src/main/java/com/canaiguess/api/config/CorsConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import java.util.List;
 
@@ -13,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:8081", "https://canaiguess.netlify.app/")); // frontend origin
+        config.setAllowedOrigins(List.of("http://localhost:8081", "https://canaiguess.netlify.app")); // frontend origin
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
@@ -22,5 +23,10 @@ public class CorsConfig {
         source.registerCorsConfiguration("/**", config);
 
         return source;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        return new CorsFilter(corsConfigurationSource());
     }
 }
